2fc8fc22420a5ebac264a6aa40280d7f96668a00
srinadhd1999 <59526428+srinadhd1999@users.noreply.github.com>
2025-03-26 09:43:12 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type {Metadata} from "next";
 import "./globals.css";
 import {BackgroundWave} from "@/components/background-wave";
 import Link from "next/link";
-import {ElevenLabsLogo, GithubLogo} from "@/components/logos";
 
 export const metadata: Metadata = {
     title: "Spark Interview Assistant",
